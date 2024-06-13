9d67e17df3c1ff91ff724f5cf777d09363998a95
Tommy Bair <tommy.bair@convergetechs.com>
2024-06-13 14:38:30 -0400

    Adds script to setup enviorment

---
+++ setup-env.sh
@@ -0,0 +1,41 @@
+
+L4T_DIR=$1
+
+# check that the path is provided
+if [ -z "$L4T_DIR" ]; then
+    echo "Please provide the path to the Linux_for_Tegra directory"
+    exit 1
+fi
+
+GLIBC_FOLDER=$(pwd)/glibc
+
+# check if glibc folder exists
+if [ ! -d "$GLIBC_FOLDER" ]; then
+    mkdir $GLIBC_FOLDER
+    wget https://developer.nvidia.com/downloads/embedded/l4t/r36_release_v3.0/toolchain/aarch64--glibc--stable-2022.08-1.tar.bz2 -O $GLIBC_FOLDER/aarch64--glibc--stable-2022.08-1.tar.bz2
+    tar -xvf $GLIBC_FOLDER/aarch64--glibc--stable-2022.08-1.tar.bz2 -C $GLIBC_FOLDER --strip-components=1
+fi
+
+CROSS_COMPILE=$GLIBC_FOLDER/bin/aarch64-buildroot-linux-gnu-
+INSTALL_MOD_PATH=$L4T_DIR/rootfs
+KERNEL_OUTPUT=$(pwd)/kernel_out
+KERNEL_DEF_CONFIG=dsboard_agx_defconfig
+
+OOT_SOURCE_LIST="nvethernetrm  nvgpu  nvidia-oot hwpm hardware nvdisplay"
+KERNEL_MODULAR_BUILD="y"
+
+export NPROC=$(($(nproc)-2))
+export LOCALVERSION=-tegra
+export ARCH=arm64 
+export KERNEL_HEADERS=$PWD/kernel/kernel-jammy-src
+export CROSS_COMPILE
+export INSTALL_MOD_PATH
+export KERNEL_OUTPUT
+export KERNEL_DEF_CONFIG
+export OOT_SOURCE_LIST
+export KERNEL_MODULAR_BUILD
+export L4T_DIR
+
+
+
+
